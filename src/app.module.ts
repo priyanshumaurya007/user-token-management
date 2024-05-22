d832cb5ab5507324614c00d6f7601b8692daaa47
Priyanshu <priyanshumaurya007@gmail.com>
2024-05-22 14:56:42 +0530

    Added .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TokenInfoModule } from './modules/token-info/token-info.module';
@@ -7,13 +8,16 @@ import { TokenInfo } from './modules/token-info/entities/token-info.entity';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'root',
-      database: 'token_info',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [TokenInfo],
       synchronize: true,
     }),
